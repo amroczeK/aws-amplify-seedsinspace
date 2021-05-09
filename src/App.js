581febb85de5a0f05680fa1b4d9768b622a3db76
Ben Nicholls <ben.nicholls@team.telstra.com>
2021-05-09 08:42:40 +0000

    added logged in status logic for displaying routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home, Dashboard, SignIn, SignUp, SeedSetUp } from "./pages";
+import { Home, SignIn, SignUp, SeedSetUp } from "./pages";
 import styled from "styled-components";
-
-// import { Header } from "./components/Nav";
+import { UserNav } from "./components/Nav/UserNav";
+import { PublicNav } from "./components/Nav/PublicNav";
 
 const AppContainer = styled.div`
   height: 100vh;
 `;
 
 const App = () => {
+  // Put this somewhere else like in context
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <AppContainer>
-      <Router>
-        {/* <Header /> */}
-        <Switch>
-          <Route exact path="/signin" component={SignIn} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/seed-setup" component={SeedSetUp} />
-          <Route exact path="/" component={Home} />
-          <Route path="/dashboard" component={Dashboard} />
-        </Switch>
-        {/* <Footer /> */}
-      </Router>
+      {loggedIn && (
+        <div>
+          <UserNav />
+          <Router>
+            <Switch>
+              <Route exact path="/seed-setup" component={SeedSetUp} />
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </Router>
+        </div>
+      )}
+      {!loggedIn && (
+        <div>
+          <PublicNav />
+          <Router>
+            <Switch>
+              <Route exact path="/signin" component={SignIn} />
+              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/" component={<div>Public Home</div>} />
+            </Switch>
+          </Router>
+        </div>
+      )}
     </AppContainer>
   );
 };
