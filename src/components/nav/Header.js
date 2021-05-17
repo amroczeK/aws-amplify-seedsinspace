800770b5b0a9bf4d188e57f517e3fecf646bdb99
Ben Nicholls <ben.nicholls@team.telstra.com>
2021-05-17 02:30:47 +0000

    small refactor

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,14 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-
+import { StyledLink } from "../styled-components/Links";
 import SideMenu from "./SideMenu";
 
 const Header = () => {
   return (
     <Container>
       <SideMenu />
-      <StyledLink to="/">
+      <StyledLink color="white" to="/">
         <h2>Seeds in space</h2>
       </StyledLink>
     </Container>
@@ -19,18 +18,8 @@ export default Header;
 
 const Container = styled.div`
   display: flex;
-  flex-direction: row;
   align-items: center;
   padding: 1em;
   gap: 2em;
-  background: ${({ theme }) => theme.primaryBackground};
-`;
-
-const StyledLink = styled(Link)`
-  justify-content: center;
-  align-items: center;
-  text-decoration: none !important;
-  h2 {
-    color: ${({ theme }) => theme.primaryLight};
-  }
+  background-color: ${({ theme }) => theme.primaryBackground};
 `;
---
+++ src/components/nav/SideMenu.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import Dialog from "@material-ui/core/Dialog";
+import IconButton from "@material-ui/core/IconButton";
+import Slide from "@material-ui/core/Slide";
+import styled from "styled-components";
+import { Menu } from "@styled-icons/heroicons-solid/Menu";
+import { Close } from "@styled-icons/evaicons-solid/Close";
+import { StyledTypography } from "../styled-components/Typography";
+import { SideMenuItems } from "./SideMenuItems";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="right" ref={ref} {...props} />;
+});
+
+const StyledMenuIcon = styled(Menu)`
+  color: ${({ theme }) => theme.primaryLight};
+  width: 1.5em;
+  height: 1.5em;
+`;
+
+const StyledCloseIcon = styled(Close)`
+  color: ${({ theme }) => theme.primaryLight};
+  width: 1.5em;
+  height: 1.5em;
+`;
+
+const StyledAppBar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 1em;
+  padding-left: 2em;
+  background: ${({ theme }) => theme.primaryBackground};
+`;
+
+const StyledMenuContent = styled.div`
+  padding: 1em 0em 0em 1em;
+  height: 100%;
+  background: ${({ theme }) => theme.primaryBackground};
+`;
+
+const SideMenu = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    console.log("hello");
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <IconButton onClick={handleOpen}>
+        <StyledMenuIcon />
+      </IconButton>
+      <Dialog fullScreen open={open} TransitionComponent={Transition}>
+        <StyledAppBar>
+          <StyledTypography variant="h5">Seeds in Space</StyledTypography>
+          <IconButton onClick={handleClose}>
+            <StyledCloseIcon />
+          </IconButton>
+        </StyledAppBar>
+        <StyledMenuContent>
+          <SideMenuItems callback={handleClose} />
+        </StyledMenuContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default SideMenu;
