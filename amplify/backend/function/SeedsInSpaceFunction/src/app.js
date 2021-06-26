945162f459d6c6a536558fe6009166804d628b82
Adrian Mroczek <adrian.mroczek@team.telstra.com>
2021-06-26 23:55:50 +0800

    update apis

--- amplify/backend/function/SeedsInSpaceFunction/src/app.js
+++ amplify/backend/function/SeedsInSpaceFunction/src/app.js
@@ -1,233 +1,254 @@
-/*
-Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-    http://aws.amazon.com/apache2.0/
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
-
-
-
-const AWS = require('aws-sdk')
-var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
-var bodyParser = require('body-parser')
-var express = require('express')
+const AWS = require("aws-sdk");
+var awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
+var bodyParser = require("body-parser");
+var express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 AWS.config.update({ region: process.env.TABLE_REGION });
 
-const dynamodb = new AWS.DynamoDB.DocumentClient();
+const db = new AWS.DynamoDB.DocumentClient();
 
 let tableName = "SeedsInSpaceTable";
-if(process.env.ENV && process.env.ENV !== "NONE") {
-  tableName = tableName + '-' + process.env.ENV;
+if (process.env.ENV && process.env.ENV !== "NONE") {
+  tableName = tableName + "-" + process.env.ENV;
 }
 
-const userIdPresent = false; // TODO: update in case is required to use that definition
-const partitionKeyName = "Pk";
-const partitionKeyType = "S";
-const sortKeyName = "Sk";
-const sortKeyType = "S";
-const hasSortKey = sortKeyName !== "";
-const path = "/seeds";
-const UNAUTH = 'UNAUTH';
-const hashKeyPath = '/:' + partitionKeyName;
-const sortKeyPath = hasSortKey ? '/:' + sortKeyName : '';
 // declare a new express app
-var app = express()
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
+var app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
 
 // Enable CORS for all methods
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next()
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
 });
+
+// Get the users unique cognito pool identifier sub
+const getUserId = req => {
+  try {
+    const reqContext = req.apiGateway.event.requestContext;
+    const authProvider = reqContext.identity.cognitoAuthenticationProvider;
+    return authProvider ? authProvider.split(":CognitoSignIn:").pop() : "UNAUTH";
+  } catch (error) {
+    console.log("Error - getUserId():", error);
+    return "UNAUTH";
+  }
+};
 
 // convert url string param to expected Type
 const convertUrlType = (param, type) => {
-  switch(type) {
+  switch (type) {
     case "N":
       return Number.parseInt(param);
     default:
       return param;
   }
-}
+};
+// params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
 
-/********************************
- * HTTP Get method for list objects *
- ********************************/
-
-app.get(path + hashKeyPath, function(req, res) {
-  var condition = {}
-  condition[partitionKeyName] = {
-    ComparisonOperator: 'EQ'
-  }
-
-  if (userIdPresent && req.apiGateway) {
-    condition[partitionKeyName]['AttributeValueList'] = [req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH ];
-  } else {
-    try {
-      condition[partitionKeyName]['AttributeValueList'] = [ convertUrlType(req.params[partitionKeyName], partitionKeyType) ];
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-
-  let queryParams = {
+/**
+ * @desc    Fetch all seed entries
+ * @route   GET /seeds
+ * @access  Public
+ */
+app.get("/seeds", async function (req, res) {
+  const params = {
     TableName: tableName,
-    KeyConditions: condition
-  }
+  };
 
-  dynamodb.query(queryParams, (err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.json({error: 'Could not load items: ' + err});
-    } else {
-      res.json(data.Items);
-    }
-  });
+  try {
+    const result = await db.scan(params).promise();
+    res.json({
+      statusCode: 200,
+      url: req.url,
+      body: JSON.stringify(result.Items),
+    });
+  } catch (error) {
+    res.json({
+      statusCode: 500,
+      error: error.message,
+    });
+  }
 });
 
-/*****************************************
- * HTTP Get method for get single object *
- *****************************************/
+/**
+ * @desc    Fetch seed entry using Primary Key (PK + SK)
+ * @route   GET /seeds/:entry_id/:type
+ * @access  Public
+ */
+app.get("/seeds/:entry_id/:type", async function (req, res) {
+  const { entry_id, type } = req.params;
 
-app.get(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
-  var params = {};
-  if (userIdPresent && req.apiGateway) {
-    params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  } else {
-    params[partitionKeyName] = req.params[partitionKeyName];
-    try {
-      params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
+  if (!entry_id || !type) {
+    res.json({
+      statusCode: 400,
+      error: "entry_id and type is required to make this request.",
+    });
   }
 
-  let getItemParams = {
+  const params = {
     TableName: tableName,
-    Key: params
-  }
+    Key: {
+      entry_id,
+      type,
+    },
+  };
 
-  dynamodb.get(getItemParams,(err, data) => {
-    if(err) {
-      res.statusCode = 500;
-      res.json({error: 'Could not load items: ' + err.message});
-    } else {
-      if (data.Item) {
-        res.json(data.Item);
-      } else {
-        res.json(data) ;
-      }
-    }
-  });
+  try {
+    const result = await db.get(params).promise();
+    res.json({
+      statusCode: 200,
+      url: req.url,
+      body: JSON.stringify(result.Item),
+    });
+  } catch (error) {
+    res.json({
+      statusCode: 500,
+      error: error.message,
+    });
+  }
 });
 
-
-/************************************
-* HTTP put method for insert object *
-*************************************/
-
-app.put(path, function(req, res) {
-
-  if (userIdPresent) {
-    req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  }
-
-  let putItemParams = {
+/**
+ * @desc    Add seed entry to dynamodb
+ * @route   POST /seeds
+ * @access  Private
+ */
+app.post("/seeds", async function (req, res) {
+  const timestamp = new Date().toISOString();
+  const params = {
     TableName: tableName,
-    Item: req.body
+    Item: {
+      ...req.body,
+      entry_id: uuidv4(),
+      createdAt: timestamp,
+      updatedAt: timestamp,
+      school_id: getUserId(req),
+    },
+  };
+
+  try {
+    const result = await db.put(params).promise();
+    res.json({
+      statusCode: 200,
+      url: req.url,
+      body: JSON.stringify(params.Item),
+    });
+  } catch (error) {
+    console.log("Add Seeds Entry Error:", error);
+    res.json({
+      statusCode: 500,
+      error: error.message,
+      url: req.url,
+    });
   }
-  dynamodb.put(putItemParams, (err, data) => {
-    if(err) {
-      res.statusCode = 500;
-      res.json({error: err, url: req.url, body: req.body});
-    } else{
-      res.json({success: 'put call succeed!', url: req.url, data: data})
-    }
-  });
 });
 
-/************************************
-* HTTP post method for insert object *
-*************************************/
+/**
+ * @desc    Update seed entry using Primary Key (PK + SK)
+ * @route   PUT /seeds/:entry_id/:type
+ * @access  Private
+ */
+app.put(`/seeds/:entry_id/:type`, async function (req, res) {
+  const { entry_id, type } = req.params;
+  const Attributes = { ...req.body };
 
-app.post(path, function(req, res) {
-
-  if (userIdPresent) {
-    req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
+  if (!entry_id) {
+    res.json({
+      statusCode: 400,
+      error: "entry_id is required to make this request.",
+    });
   }
 
-  let putItemParams = {
+  const timestamp = new Date().toISOString();
+
+  let params = {
     TableName: tableName,
-    Item: req.body
+    Key: {
+      entry_id,
+      type,
+    },
+    UpdateExpression: "SET ", // Start of update expression before dynamic population
+    ExpressionAttributeNames: {},
+    ExpressionAttributeValues: {},
+    ReturnValues: "UPDATED_NEW", // Return updated attributes in result response
+  };
+
+  if (type) {
+    params.UpdateExpression += `#${type} = :${type}, `;
+    params.ExpressionAttributeNames[`#${type}`] = type; // Type is a dynamodb reserved keyword
+    params.ExpressionAttributeValues[`:${type}`] = type;
   }
-  dynamodb.put(putItemParams, (err, data) => {
-    if(err) {
-      res.statusCode = 500;
-      res.json({error: err, url: req.url, body: req.body});
-    } else{
-      res.json({success: 'post call succeed!', url: req.url, data: data})
-    }
-  });
+  for (const attr in Attributes) {
+    params.UpdateExpression += `${attr} = :${attr}, `;
+    params.ExpressionAttributeValues[`:${attr}`] = Attributes[attr];
+  }
+  params.UpdateExpression += "updatedAt = :updatedAt";
+  params.ExpressionAttributeValues[":updatedAt"] = timestamp;
+
+  try {
+    const result = await db.update(params).promise();
+    res.json({
+      statusCode: 200,
+      url: req.url,
+      body: `Updated seed entry ${entry_id} with ${result}`,
+    });
+  } catch (error) {
+    res.json({
+      statusCode: 500,
+      error: error.message,
+      url: req.url,
+    });
+  }
 });
 
-/**************************************
-* HTTP remove method to delete object *
-***************************************/
+/**
+ * @desc    Delete seed entry using Primary Key (PK + SK)
+ * @route   DELETE /seeds/:entry_id/:type
+ * @access  Private
+ */
+app.delete("/seeds/:entry_id/:type", async function (req, res) {
+  const { entry_id, type } = req.params;
 
-app.delete(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
-  var params = {};
-  if (userIdPresent && req.apiGateway) {
-    params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  } else {
-    params[partitionKeyName] = req.params[partitionKeyName];
-     try {
-      params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
+  if (!entry_id) {
+    res.json({
+      statusCode: 400,
+      error: "entry_id is required to make this request.",
+    });
   }
 
-  let removeItemParams = {
+  const params = {
     TableName: tableName,
-    Key: params
+    Key: {
+      entry_id,
+      type,
+    },
+  };
+
+  try {
+    const result = await db.delete(params).promise();
+    res.json({
+      statusCode: 200,
+      url: req.url,
+      body: `Deleted seed entry ${entry_id}`,
+    });
+  } catch (error) {
+    console.log("Delete Seed Entry Error:", error);
+    res.json({
+      statusCode: 500,
+      error: error.message,
+      url: req.url,
+    });
   }
-  dynamodb.delete(removeItemParams, (err, data)=> {
-    if(err) {
-      res.statusCode = 500;
-      res.json({error: err, url: req.url});
-    } else {
-      res.json({url: req.url, data: data});
-    }
-  });
 });
-app.listen(3000, function() {
-    console.log("App started")
+
+app.listen(3000, function () {
+  console.log("App started");
 });
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
-module.exports = app
+module.exports = app;
