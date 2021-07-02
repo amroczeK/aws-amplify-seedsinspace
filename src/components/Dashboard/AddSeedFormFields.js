29f1c96dfe215fec764707346eee132ca64d7c80
Ben Nicholls <ben.nicholls@team.telstra.com>
2021-07-02 05:59:17 +0000

    Moved dashboard content/components to components/Dashboard

---
+++ src/components/Dashboard/AddSeedFormFields.js
@@ -0,0 +1,157 @@
+import { useState } from "react";
+import { Controller } from "react-hook-form";
+import styled from "styled-components";
+import CardContent from "@material-ui/core/CardContent";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import { InputAdornment } from "@material-ui/core";
+import { StyledButton } from "../styled-components/Buttons";
+import { StyledInputLabel } from "../styled-components/InputLabel";
+import ImageUpload from "../ImageUpload";
+import RemoveEntryModal from "./RemoveEntryModal";
+
+const AddSeedFormFields = ({ name, control, setValue, errors }) => {
+  const [open, setModalOpen] = useState(false);
+
+  return (
+    <div style={{ display: "grid" }}>
+      <SeedHeader>
+        <Typography variant="subtitle2" style={{ fontWeight: "600" }}>
+          SEED NAME
+        </Typography>
+        <Typography variant="h6" style={{ fontWeight: "bold", color: "#fff" }}>
+          {name}
+        </Typography>
+      </SeedHeader>
+
+      <GroupLabel variant="subtitle2">PHOTOS</GroupLabel>
+      <StyledSeedInfo>
+        <ImageUpload
+          name="image" // need to add date to the name
+          preview={false}
+          path="protected/"
+          level="protected"
+          text="Add photo"
+          setValue={setValue}
+        />
+      </StyledSeedInfo>
+
+      <GroupLabel variant="subtitle2">SEED DATA</GroupLabel>
+      <StyledSeedInfo>
+        <StyledInputLabel shrink>HEIGHT</StyledInputLabel>
+        <Controller
+          name="height"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              error={errors?.height ? true : false}
+              helperText={errors?.height?.message || null}
+              InputProps={{
+                endAdornment: <InputAdornment>millimeter</InputAdornment>,
+              }}
+            />
+          )}
+        />
+        <StyledInputLabel shrink>STEM LENGTH</StyledInputLabel>
+        <Controller
+          name="stemLength"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              error={errors?.stemLength ? true : false}
+              helperText={errors?.stemLength?.message || null}
+              InputProps={{
+                endAdornment: <InputAdornment>millimeter</InputAdornment>,
+              }}
+            />
+          )}
+        />
+        <StyledInputLabel shrink>LEAF SIZE</StyledInputLabel>
+        <Controller
+          name="leafSize"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              error={errors?.leafSize ? true : false}
+              helperText={errors?.leafSize?.message || null}
+              InputProps={{
+                endAdornment: <InputAdornment>millimeter</InputAdornment>,
+              }}
+            />
+          )}
+        />
+        <StyledInputLabel shrink>LEAF COLOR</StyledInputLabel>
+        <Controller
+          name="leafColor"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              error={errors?.leafColor ? true : false}
+              helperText={errors?.leafColor?.message || null}
+            />
+          )}
+        />
+        <StyledButton color="primary">
+          <b>+ Add new data measure</b>
+        </StyledButton>
+      </StyledSeedInfo>
+
+      <GroupLabel variant="subtitle2">NOTES</GroupLabel>
+      <StyledSeedInfo>
+        <Controller
+          name="notes"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              variant="outlined"
+              multiline
+              rows={10}
+              error={errors?.notes ? true : false}
+              helperText={errors?.notes?.message || null}
+            />
+          )}
+        />
+      </StyledSeedInfo>
+
+      <StyledButton color="primary" padding="1em 1em" onClick={() => setModalOpen(true)}>
+        <b>Stop recording data for this seed</b>
+      </StyledButton>
+      <RemoveEntryModal open={open} close={() => setModalOpen(false)} name={name} />
+    </div>
+  );
+};
+
+export default AddSeedFormFields;
+
+const GroupLabel = styled(Typography)`
+  font-weight: bold;
+  color: #9e9e9e;
+  background-color: #e0e0e0;
+  padding: 0.5em 1.5em;
+`;
+
+const SeedHeader = styled(CardContent)`
+  padding: 1.5em;
+  background-color: #9e9e9e;
+`;
+
+const StyledSeedInfo = styled(CardContent)`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  padding: 1em 1.5em;
+  background-color: #fff;
+  &:last-child {
+    border-top: 1px solid #e0e0e0;
+    padding: 0em;
+  }
+`;
