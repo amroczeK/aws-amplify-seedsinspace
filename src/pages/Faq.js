1fbf3dd34a03df2f6ca765c93a4d992399fa9abf
Kasfia Parvez <Kasfia.Parvez@team.telstra.com>
2021-07-09 10:02:59 +1000

    Faq

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,138 +1,68 @@
-import styled from 'styled-components'
-import React, {useState, useContext} from 'react'
+import styled from "styled-components";
+import React, { useState } from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
 
-const ToggleContext = React.createContext()
 
-function Accordion({ children, ...restProps }){
-  return (
-    <Container {...restProps}>{children}</Container>
-  )
-}
-
-//Frame, Item, Header, Title, Body
-Accordion.Frame = ({ children, ...restProps }) => (
-  <Frame {...restProps}> {children} </Frame>
-)
-
-Accordion.Item = function AccordionItem({ children, ...restProps }) {
-
-  const [toggle, setToggle] = useState(false)
+const AccordionPanel = ({ children, title }) => {
+  const [open, setOpen] = useState(false);
 
   return (
-    <ToggleContext.Provider value={{ toggle, setToggle }}>
-      <Item {...restProps}> {children} </Item>
-    </ToggleContext.Provider>
-  )
-}
-
-Accordion.Header = function AccordionHeader({ children, ...restProps }) {
-
-  const {toggle, setToggle} = useContext(ToggleContext)
-
-  return (
-      <Header {...restProps} onClick={() => setToggle(!toggle)}>
-        {children}
-        {toggle ? <h1> - </h1> : <h1> + </h1>}
-      </Header>
-    )
-}
-
-Accordion.Title = function AccordionTitle({ children, ...restProps }) {
-  return <Title {...restProps}> {children} </Title>
-}
-
-Accordion.Body = function AccordionBody({ children, ...restProps }) {
-
-  const {toggle} = useContext(ToggleContext)
-
-  return toggle ? <Body {...restProps}> {children} </Body> : null;
-}
-
-const faqs = [
-
-    {
-        "id": 1,
-        "header": "Will aliens grow from the space seeds?",
-        "body": "We don’t think so."
-    },
-    {
-        "id": 2,
-        "header": "How do I submit our application?",
-        "body": "Everyone must use the online submission form."
-    },
-    {
-        "id": 3,
-        "header": "Who can participate?",
-        "body": "This is open to all Australian schools or education institutions or home schools for students aged 3 – 25."
-    },
-    {
-        "id": 4,
-        "header": "Can home-schooled students enter?",
-        "body": "Yes"
-    },
-    {
-        "id": 5,
-        "header": "When will the seeds be back?",
-        "body": "On SpaceX 22 which is scheduled for a July 2021 splashdown."
-    },
-    {
-      "id": 6,
-      "header": "Who do I contact if I have questions?",
-      "body": "Send an email to info@onegiantleapaustralia.com"
-    }
-]
-
-export default function Faq() {
-  return (
-    <>
-    <h1 style={{textAlign: 'center', margin: "40px"}}>FAQ</h1>
-      <Accordion>
-        {faqs.map( (item) => (
-        <Accordion.Frame>
-          <Accordion.Item>
-            <Accordion.Header>
-              <Accordion.Title>{item.header}</Accordion.Title>
-            </Accordion.Header>
-            <Accordion.Body>{item.body}</Accordion.Body>
-          </Accordion.Item>
-        </Accordion.Frame>
-        ))}
-      </Accordion>
-    </>
+    <div>
+      <AccordionHeader onClick={() => setOpen(o => !o)}>
+        {title}
+        <div style={{ textAlign: "end", flex: "1 0" }}>{open ? "-" : "+"}</div>
+      </AccordionHeader>
+      {open && <AccordionContent>{children}</AccordionContent>}
+    </div>
   );
-}
+};
 
+const FAQ = () => {
+  return (
+    <Container maxWidth="md">
+      <h1 style={{ textAlign: "center", margin: "40px" }}>FAQ</h1>
+      <AccordionPanel title="Will aliens grow from the space seeds?">
+        <StyledTypography>We don’t think so.</StyledTypography>
+      </AccordionPanel>
+      <AccordionPanel title="How do I submit our application?">
+        <StyledTypography>Everyone must use the online submission form.</StyledTypography>
+      </AccordionPanel>
+      <AccordionPanel title="Who can participate?">
+        <StyledTypography>
+          This is open to all Australian schools or education institutions or home schools
+          for students aged 3 – 25.
+        </StyledTypography>
+      </AccordionPanel>
+      <AccordionPanel title="Can home-schooled students enter?">
+        <StyledTypography>Absolutely!</StyledTypography>
+      </AccordionPanel>
+      <AccordionPanel title="When will the seeds be back?">
+        <StyledTypography>On SpaceX 22 which is scheduled for a July 2021 splashdown.</StyledTypography>
+      </AccordionPanel>
+      <AccordionPanel title="Who do I contact if I have questions?">
+        <StyledTypography>Send an email to info@onegiantleapaustralia.com</StyledTypography>
+      </AccordionPanel>
+    </Container>
+  );
+};
 
-const Container = styled.div`
-  width: 600px;
-  border: 1px solid white;
-  margin: 0 auto;
-`
-
-//Frame, Item, Header, Title, Body
-const Frame = styled.div`
-  border: 1px solid #358C5F;
-`
-
-const Item = styled.div`
-  & {
-    cursor: pointer;
-  }
-`
-
-const Header = styled.div`
+const AccordionHeader = styled.h2`
+  border: 1px solid green;
+  padding: 1em;
+  margin: 0;
   display: flex;
-  justify-content: space-between;
-  padding: 0 1rem;
-  align-items: center;
-`
+`;
 
-const Title = styled.h2`
-  font-size: 25px;
-  color: var(--color2);
-`
+const AccordionContent = styled.div`
+  border: 1px solid green;
+  padding: 1em;
+`;
 
-const Body = styled.h3`
-  padding: 1rem;
-  border-top: 1px solid #6BBE93;
-`
+const StyledTypography = styled(Typography)`
+  font-weight: 550;
+  text-decoration: none;
+  margin: 10px;
+`;
+
+export default FAQ;
