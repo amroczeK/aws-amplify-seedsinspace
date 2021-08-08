185633ad7bcb68901fd0417392166afa3c57b8f5
amroczeK <mroczek9990@gmail.com>
2021-08-08 22:19:48 +0800

    responsive weather and add seed button

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { format } from "date-fns";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import { useQuery } from "react-query";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import styled from "styled-components";
@@ -12,14 +10,12 @@ const WeatherApp = () => {
   const date = new Date();
 
   return (
-    <Card>
-      <CardContent>
-        <Typography style={{ fontWeight: "bold" }} variant="h6">
-          {format(date, "eeee dd MMMM yyyy")}
-        </Typography>
-        <Weather />
-      </CardContent>
-    </Card>
+    <Container>
+      <Typography style={{ fontWeight: "bold" }} variant="h6">
+        {format(date, "eeee dd MMMM yyyy")}
+      </Typography>
+      <Weather />
+    </Container>
   );
 };
 
@@ -69,9 +65,16 @@ const Weather = () => {
   );
 };
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
 const StyledWeather = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  align-items: left;
   gap: 1.5em;
   padding: 1em 0em 0em 0em;
 `;
