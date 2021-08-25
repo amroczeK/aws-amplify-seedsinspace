aab1fe6e8f3660a58cb0c028e7735231202b426d
amroczeK <mroczek9990@gmail.com>
2021-08-25 21:29:54 +0800

    Revert "added content to community page"
    This reverts commit 89b36fb450aee03315176f1e9cd96cf9828a60af.

--- src/pages/ParticipatingSchools.js
+++ src/pages/ParticipatingSchools.js
@@ -12,15 +12,7 @@ const ParticipatingSchools = () => {
   }, []);
 
   return (
-    <Container style={{ paddingTop: "1em" }} maxWidth="xl">
-      <h1>Our community</h1>
-      <p>
-        We have over 250 participating groups from all around Australia, including primary
-        schools, high schools and scout groups.
-      </p>
-      <p>
-        Zoom in to the map to take a look at the participating groups and their seed data.
-      </p>
+    <Container style={{ paddingTop: "2em" }} maxWidth="xl">
       <StepperContent schools={schools} />
     </Container>
   );
