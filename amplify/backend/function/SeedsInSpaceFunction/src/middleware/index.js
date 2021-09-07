a4443b0959b0c81ca8d26452ad33e52990b7e9e5
Adrian Mroczek <adrian.mroczek@team.telstra.com>
2021-09-07 16:08:44 +0800

    adding rate limiting

--- amplify/backend/function/SeedsInSpaceFunction/src/middleware/index.js
+++ amplify/backend/function/SeedsInSpaceFunction/src/middleware/index.js
@@ -1,3 +1,11 @@
+const rateLimit = require("express-rate-limit");
+
+const rateLimiter = rateLimit({
+  //windowMs: 1 * 60 * 1000, // 1 minutes
+  windowMs: 30 * 1000, // 30 seconds
+  max: 50, // limit each IP to 50 requests per windowMs
+});
+
 const auth = async (req, res, next) => {
   try {
     const reqContext = req.apiGateway.event.requestContext;
@@ -21,4 +29,5 @@ const auth = async (req, res, next) => {
 
 module.exports = {
   auth,
+  rateLimiter
 };
