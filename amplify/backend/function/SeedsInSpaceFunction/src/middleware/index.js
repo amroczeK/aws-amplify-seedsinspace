37d9e31c62d8d5af82629bf3ae335a865cd42ef0
Adrian Mroczek <adrian.mroczek@team.telstra.com>
2021-09-08 08:05:06 +0800

    test small limit in AWS env

--- amplify/backend/function/SeedsInSpaceFunction/src/middleware/index.js
+++ amplify/backend/function/SeedsInSpaceFunction/src/middleware/index.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const rateLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
-  max: 50, // limit each IP to 50 requests per windowMs
+  max: 3, // limit each IP to 50 requests per windowMs
 });
 
 const auth = async (req, res, next) => {
