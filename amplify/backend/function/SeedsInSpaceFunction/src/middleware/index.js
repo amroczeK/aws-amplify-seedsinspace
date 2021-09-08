9c668d3eb07c58d6b7ea802a585b90e41edb663d
Adrian Mroczek <adrian.mroczek@team.telstra.com>
2021-09-08 08:03:31 +0800

    increase window

--- amplify/backend/function/SeedsInSpaceFunction/src/middleware/index.js
+++ amplify/backend/function/SeedsInSpaceFunction/src/middleware/index.js
@@ -1,8 +1,7 @@
 const rateLimit = require("express-rate-limit");
 
 const rateLimiter = rateLimit({
-  //windowMs: 1 * 60 * 1000, // 1 minutes
-  windowMs: 30 * 1000, // 30 seconds
+  windowMs: 1 * 60 * 1000, // 1 minutes
   max: 50, // limit each IP to 50 requests per windowMs
 });
 
